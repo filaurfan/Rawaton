ceee75796bf4d8425c5e6e56ba2cb68b70134c80
basribasren <basri.official.acc@gmail.com>
2017-01-23 10:40:45 +0700

    link ke halaman lain

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,36 @@ function ensureAuthenticated(req, res, next){
 	}
 }
 
+// router.get('/register', function(req, res){
+// 	res.render('register');
+// });
+
+// router.get('/login', function(req, res){
+// 	res.render('login');
+// });
+
+router.get('/index', function(req, res){
+	res.render('index');
+});
+
+router.get('/about', function(req, res){
+	res.render('about');
+});
+
+router.get('/contact', function(req, res){
+	res.render('contact');
+});
+
+router.get('/news', function(req, res){
+	res.render('news');
+});
+
+router.get('/preview', function(req, res){
+	res.render('preview');
+});
+
+router.get('/delivery', function(req, res){
+	res.render('delivery');
+});
+
 module.exports = router;
